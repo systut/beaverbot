0a5b6ce4e7ec22c9a51d8a9c938a0914c2caee4f
chuoru <chuoru@github.com>
2024-09-17 18:51:34 +0900

    :+1+ update rules

--- beaverbot_dockerfiles/rules/register.sh
+++ beaverbot_dockerfiles/rules/register.sh
@@ -1,4 +1,5 @@
-sudo cp 99-usb-camera.rules /etc/udev/rules.d/
-sudo cp 99-mmal-camera.rules /lib/udev/rules.d/
+sudo cp 99-usb-driver.rules /etc/udev/rules.d/
+sudo cp 99-usb-imu.rules /etc/udev/rules.d/
+sudo cp 99-usb-gps.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
